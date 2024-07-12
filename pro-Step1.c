d0aa850703206e26652fc0ec66b7a16d0bcf488f
malaviyapunam <159758998+malaviyapunam@users.noreply.github.com>
2024-07-12 13:09:04 +0530

    Add files via upload

---
+++ pro-Step1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	int per;
+	printf("Enter Your per:");
+	scanf("%d",&per);
+	
+	char grade=(per>=90)?'A':
+	           (per>=80)?'B':
+	           (per>=70)?'C':
+			   (per>=60)?'D':
+			   (per>=35)?'E':
+			   'F';
+	
+	printf("Your grade is %c\n",grade);
+	
+}
